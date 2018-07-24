2ae8adf93b3e3324620d9b36e34d87fee25af218
mark.z <norrain@qq.com>
2018-07-24 14:13:53 +0800

    如果不使用显示名称注册 则必须使用注解注册及服务

--- mrpc-core/src/main/java/com/mrpc/core/client/MrpcClient.java
+++ mrpc-core/src/main/java/com/mrpc/core/client/MrpcClient.java
@@ -100,12 +100,9 @@ public final class MrpcClient implements IClient {
     @Override
     public <T> T getService(final Class<T> clazz) {
         RpcService rpcService = InfectUtils.getInterFaceAnno(clazz, RpcService.class);
-        if (rpcService != null) {
-            Objects.requireNonNull(rpcService.value(), "您要使用的服务对像注解名称为空");
-            return this.getService(rpcService.value(), clazz);
-        }
-
-        return this.getService(clazz.getSimpleName(), clazz);
+        Objects.requireNonNull(rpcService, "您要使用的服务对像注解为空");
+        Objects.requireNonNull(rpcService.value(), "您要使用的服务对像注解名称为空");
+        return this.getService(rpcService.value(), clazz);
     }
 
     @Override
